1db2a4e6a94dbfcb2fc415d2fb6cc15090c33dd6
Deval1509 <109296657+Deval1509@users.noreply.github.com>
2022-07-15 12:40:48 +0100

    Hope it works

---
+++ script.js
@@ -0,0 +1,31 @@
+screen= document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = 'Use The buttons';
+
+for(item of buttons){
+    item.addEventListener('click', (e) =>{
+        buttonText=e.target.innerText;
+         console.log('button text is',buttonText);
+
+             if(buttonText== '*'){
+                 screenValue += buttonText;
+                 screen.value = screenValue;
+             }
+             else if(buttonText == "C") 
+             { 
+                screenValue = ""; 
+                screen.value = screenValue;
+
+             }
+             else if(buttonText =='='){
+                screen.value = eval(screenValue)
+             }
+             else{
+                screenValue += buttonText;
+                screen.value = screenValue;
+             }
+
+            
+
+    })
+}
